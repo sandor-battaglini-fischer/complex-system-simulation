4d0ff02f9d961c79c38237a74ed8c9c8409b2939
SpazzSpud <tntnorton88@gmail.com>
2024-02-04 23:46:14 +0100

    Create test_francesco_and_laura.py

---
+++ tests/test_francesco_and_laura.py
@@ -0,0 +1,26 @@
+import unittest
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from love.base_model import external_stress
+
+class Tests(unittest.TestCase):
+        
+    def test_love_dynamics_output_type(self):
+        y = [0.895, 1.5]
+        t = 0
+        p = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]
+        epsilon = 0.1
+        omega = 2 * np.pi / 52
+        result = love_dynamics(y, t, p, epsilon, omega)
+        self.assertIsInstance(result, list)  # or np.ndarray, depending on implementation
+
+    def test_compute_LLE_for_params_output_range(self):
+        param_tuple = (0.1, 0.2)
+        lle_value = compute_LLE_for_params(param_tuple)
+        self.assertIsInstance(lle_value, float)
+        self.assertTrue(0 <= lle_value <= 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
